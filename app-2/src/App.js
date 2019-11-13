3086b9c98e79cd6312cf990dc2d6e632f87a638d
Mimi <mimitraugott@gmail.com>
2019-11-13 14:10:25 -0700

    first commit of app-2 react drills

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,19 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      foods: ["juice", "milk", "cheese", "beans"]
+    }
+  }
+
+
   render() {
+    let foodsToDisplay = this.state.foods.map((el,i) => {
+      return <h1 key={index}>{element}</h1>
+    })
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div className="App">{foodsToDisplay}</div>
     );
   }
 }
