3b9607f7b431dac846315a446b7333e0a9ec6d4f
Mimi <mimitraugott@gmail.com>
2019-11-13 17:27:08 -0700

    first push of 7

--- app-7/src/App.js
+++ app-7/src/App.js
@@ -1,18 +1,29 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import NewTask from '../Components/NewTask';
+import List from '../Components/List';
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super();
+
+    this.state={
+      list:[]
+    };
+    this.handleTask = this.handleTask.bind(this)
+  }
+  handleAddTask (task) {
+    this.setState({ list: [...this.state.list, task ] })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>MY LIST OF ADULTING TO-DO</h1>
+
+        <NewTask add={this.handleAddTask}/>
+        <List tasks={this.state.list}/>
+       
       </div>
     );
   }
